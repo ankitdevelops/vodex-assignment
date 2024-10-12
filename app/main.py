6fc59f63af5b5742229986139907774708c66df4
Ankit Kumar <heyankitkr@gmail.com>
2024-10-12 10:22:54 +0530

    updated requirements and port env

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+import os
 import uvicorn
+from dotenv import load_dotenv
 
+load_dotenv()
+PORT = os.getenv("PORT")
 if __name__ == "__main__":
     uvicorn.run("config.index:app", host="0.0.0.0", port=8000, reload=True)
